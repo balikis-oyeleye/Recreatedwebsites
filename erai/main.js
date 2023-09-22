6c17f92c6f8b67b3f29e83989742a21352e9b664
Qreamville <Oyeleyebalikis@yahoo.com>
2023-09-22 09:04:30 +0100

    initialize project

---
+++ erai/main.js
@@ -0,0 +1,16 @@
+import "./styles/index.scss";
+import Lenis from "@studio-freight/lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+// smooth scroll
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
