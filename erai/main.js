c364250076ffc2e9544753eef4979d6307d41f40
Qreamville <Oyeleyebalikis@yahoo.com>
2023-09-22 21:32:17 +0100

    complete

--- erai/main.js
+++ erai/main.js
@@ -14,3 +14,47 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+const timeline = gsap.timeline();
+
+gsap.to(".img-container", {
+  scale: 52,
+  ease: "ease",
+  scrollTrigger: {
+    trigger: ".video-section",
+    start: "top top",
+    end: "bottom",
+    scrub: 1,
+    pin: true,
+  },
+});
+
+gsap.to(".right", {
+  autoAlpha: 0,
+  x: 500,
+  duration: 1.5,
+  scrollTrigger: {
+    start: 1,
+  },
+});
+
+gsap.to(".left", {
+  autoAlpha: 0,
+  x: -500,
+  duration: 1.5,
+  scrollTrigger: {
+    start: 1,
+  },
+});
+
+timeline
+  .from(".title span", {
+    y: 150,
+    skewY: 10,
+    duration: 3,
+  })
+  .from(".text-bottom", {
+    letterSpacing: -10,
+    opacity: 0,
+    duration: 3,
+  });
